d9834d908d57408c46b7e9605ffae6bce0a7872d
Aleixgelabert <aleixgelabert@gmail.com>
2026-03-20 09:57:30 +0100

    Actualització carpeta SOFTWARE

--- SOFTWARE/FEATURES_V4/ICONES/components/lvgl_ui/tileview/system_tile.cpp
+++ SOFTWARE/FEATURES_V4/ICONES/components/lvgl_ui/tileview/system_tile.cpp
@@ -40,11 +40,13 @@ LV_IMG_DECLARE(wifi_3);
 
 // Variables globals per etiquetes de la interfície gràfica (LVGL)
 lv_obj_t *label_brightness;
-lv_obj_t *label_time;
-lv_obj_t *label_date;
+//lv_obj_t *label_time;
+//lv_obj_t *label_date;
 lv_obj_t *label_cylinder_pressure;
 lv_obj_t *label_cylinder_position = nullptr;
 lv_obj_t *label_cylinder_load;
+lv_obj_t *label_joystick_position_B;
+lv_obj_t *label_output_joystick_B;
 lv_obj_t *icon_battery = NULL;
 lv_obj_t *icon_wifi = NULL;
 lv_obj_t *label_batt_pct = NULL;
@@ -67,7 +69,7 @@ static void slider_event_cb(lv_event_t *e)
         lv_event_stop_bubbling(e);                               // Evita que l’event es propagui a altres objectes
     }
 }
-
+/*
 //---------------------------------------------------------
 // CALLBACK del temporitzador que actualitza hora
 //---------------------------------------------------------
@@ -91,14 +93,17 @@ static void system_status_cb(lv_timer_t *t)
     system_update_battery(pct, chg);
     system_update_wifi(system_get_wifi_level());
 }
-
+*/
 
 //---------------------------------------------------------
 // Inicialització del sistema i lectura de recursos de maquinari
 //---------------------------------------------------------
 void system_init(void)
 {
+    // Aquí pots inicialitzar sensors, PMU, RTC si cal
 }
+
+
 
 
 //---------------------------------------------------------
@@ -110,12 +115,12 @@ void system_tile_init(lv_obj_t *parent)
     lv_obj_t *lable = lv_label_create(parent);                       // Crea el títol superior
     lv_obj_set_style_text_font(lable, &lv_font_montserrat_20, LV_PART_MAIN);    // Defineix la font del text de l’etiqueta com lv_font_montserrat_20, una mida de lletra mitjana/gran.
                                                                                 // LV_PART_MAIN indica que el canvi s’aplica a la part principal de l’objecte.
-    lv_label_set_text(lable, "J2 CYLINDER");
-    lv_obj_align(lable, LV_ALIGN_TOP_MID, 0, 3);        // Col·loca el títol a la part superior i centrada horitzontalment (TOP_MID),
+    lv_label_set_text(lable, "TRIM TAB");
+    lv_obj_align(lable, LV_ALIGN_TOP_MID, 0, 10);        // Col·loca el títol a la part superior i centrada horitzontalment (TOP_MID),
                                                         // amb un petit desplaçament vertical de 3 píxels.
     lv_obj_set_size(list, lv_pct(95), lv_pct(70));      // Dona a la llista una mida del 95% de l’amplada i 70% de l’alçada del contenidor pare (parent).
                                                         // Així ocupa gairebé tota la pantalla.
-    lv_obj_align(list, LV_ALIGN_TOP_MID, 0, 30);        // Col·loca la llista sota el títol, centrada a dalt, i amb 30 píxels de marge des del top.
+    lv_obj_align(list, LV_ALIGN_TOP_MID, 0, 40);        // Col·loca la llista sota el títol, centrada a dalt, i amb 30 píxels de marge des del top.
 
     
     // Slider per controlar la brillantor
@@ -129,50 +134,62 @@ void system_tile_init(lv_obj_t *parent)
     // Elements de la llista amb diferents informacions del sistema
     lv_obj_t *list_item;
 
-    list_item = lv_list_add_btn(list, NULL, "Position");
-    label_cylinder_position = lv_label_create(list_item);
-    lv_label_set_text(label_cylinder_position, "--- %");
-
     list_item = lv_list_add_btn(list, NULL, "Brightness");
     label_brightness = lv_label_create(list_item);
     lv_label_set_text(label_brightness, "80 %");
 
-    list_item = lv_list_add_btn(list, NULL, "Load");
-    label_cylinder_load = lv_label_create(list_item);
-    lv_label_set_text(label_cylinder_load, "--- Kg");
+    list_item = lv_list_add_btn(list, NULL, "Distance");
+    label_cylinder_position = lv_label_create(list_item);
+    lv_label_set_text(label_cylinder_position, "--- cm");
 
     list_item = lv_list_add_btn(list, NULL, "Pressure");
     label_cylinder_pressure = lv_label_create(list_item);
     lv_label_set_text(label_cylinder_pressure, "--- BAR");
 
-    list_item = lv_list_add_btn(list, NULL, "Date");
+    list_item = lv_list_add_btn(list, NULL, "Load");
+    label_cylinder_load = lv_label_create(list_item);
+    lv_label_set_text(label_cylinder_load, "--- Kg");
+
+    list_item = lv_list_add_btn(list, NULL, "Output Joystick B");
+    label_output_joystick_B = lv_label_create(list_item);
+    lv_label_set_text(label_output_joystick_B, "--- mV");
+
+    list_item = lv_list_add_btn(list, NULL, "Position Joystick B");
+    label_joystick_position_B = lv_label_create(list_item);
+    lv_label_set_text(label_joystick_position_B, "--- %");
+
+    /*list_item = lv_list_add_btn(list, NULL, "Date");
     label_date = lv_label_create(list_item);
     lv_label_set_text(label_date, "XX-XX-XXXX");
 
     list_item = lv_list_add_btn(list, NULL, "Time");
     label_time = lv_label_create(list_item);
-    lv_label_set_text(label_time, "12:00:00");
+    lv_label_set_text(label_time, "12:00:00");*/
 
     system_init();                                     // Inicialitza el maquinari
-    lv_timer_create(system_time_cb, 1000, NULL);       // Temporitzador per actualitzar hora/temp cada segon
-    lv_timer_create(system_status_cb, 2000, NULL);
-
+    /*lv_timer_create(system_time_cb, 1000, NULL);       // Temporitzador per actualitzar hora/temp cada segon
+    lv_timer_create(system_status_cb, 2000, NULL);*/
 
     // Icona bateria
     icon_battery = lv_img_create(parent);
-    lv_obj_align(icon_battery, LV_ALIGN_TOP_RIGHT, -10, 5);
+    lv_obj_align(icon_battery, LV_ALIGN_TOP_RIGHT, -17, -10);
 
     // Percentatge de bateria
     label_batt_pct = lv_label_create(parent);
-    lv_label_set_text(label_batt_pct, "---%");
-    lv_obj_align(label_batt_pct, LV_ALIGN_TOP_RIGHT, -60, 12);
-    lv_obj_set_style_text_font(label_batt_pct, &lv_font_montserrat_16, 0);
+    lv_label_set_text(label_batt_pct, "");
+    lv_obj_align(label_batt_pct, LV_ALIGN_TOP_RIGHT, -10, 12);
+    lv_obj_set_style_text_font(label_batt_pct, &lv_font_montserrat_12, 0);
+
+    // -------- CANVI: Actualitzar el label amb valor real de bateria si hi ha dades --------
+    int pct = pmu_get_battery_percentage();
+    bool chg = pmu_is_charging();
+    system_update_battery(pct, chg);
 
 
     // Icona WiFi
     icon_wifi = lv_img_create(parent);
-    lv_obj_align(icon_wifi, LV_ALIGN_TOP_RIGHT, -10, 55);
-/*
+    lv_obj_align(icon_wifi, LV_ALIGN_TOP_RIGHT, -50, -10);
+
     lv_timer_create([](lv_timer_t *t){
     int pct = pmu_get_battery_percentage();
     bool chg = pmu_is_charging();
@@ -181,7 +198,7 @@ void system_tile_init(lv_obj_t *parent)
     system_update_wifi(system_get_wifi_level());
 
     }, 2000, NULL);
-*/
+
 
 }
 
@@ -206,6 +223,9 @@ void system_update_battery(int pct, bool charging)
 
     lv_img_set_src(icon_battery, img);
 
+    /* Escalar la imatge */
+    lv_img_set_zoom(icon_battery, 128);  // 50% de la mida original
+    
     // Actualitza el % al widget
     char buff[8];
     snprintf(buff, sizeof(buff), "%d%%", pct);
@@ -244,6 +264,8 @@ void system_update_wifi(int level)
             lv_img_set_src(icon_wifi, &wifi_off);
             break;
     }
+        /* Escalar la icona Wi-Fi */
+    lv_img_set_zoom(icon_wifi, 128);  // Ajusta la mida de la icona
 }
 
 
