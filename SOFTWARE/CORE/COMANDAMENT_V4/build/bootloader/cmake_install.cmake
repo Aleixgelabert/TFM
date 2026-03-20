ee0432f4ebea324d1c9d4b988f459603df5a9864
Aleixgelabert <aleixgelabert@gmail.com>
2026-03-20 10:59:47 +0100

    Canvi ESP-IDF a v5.5.3

--- SOFTWARE/CORE/COMANDAMENT_V4/build/bootloader/cmake_install.cmake
+++ SOFTWARE/CORE/COMANDAMENT_V4/build/bootloader/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/aleixgelabert/esp/v5.5.1/esp-idf/components/bootloader/subproject
+# Install script for directory: /Users/aleixgelabert/.espressif/v5.5.3/esp-idf/components/bootloader/subproject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,14 +34,20 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Users/aleixgelabert/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump")
+  set(CMAKE_OBJDUMP "/Users/aleixgelabert/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20251107/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/bootloader/esp-idf/cmake_install.cmake")
+  include("/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/bootloader/esp-idf/cmake_install.cmake")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/bootloader/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -55,8 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/bootloader/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/bootloader/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
+++ SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/aleixgelabert/esp/v5.5.1/esp-idf/components/mbedtls/mbedtls
+# Install script for directory: /Users/aleixgelabert/.espressif/v5.5.3/esp-idf/components/mbedtls/mbedtls
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,15 +34,21 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Users/aleixgelabert/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump")
+  set(CMAKE_OBJDUMP "/Users/aleixgelabert/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20251107/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
-  include("/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake")
-  include("/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
-  include("/Users/aleixgelabert/Desktop/TFM_MECATRONICA/C/VERSIO4/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/pkgconfig/cmake_install.cmake")
+  include("/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
+  include("/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake")
+  include("/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
+  include("/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/pkgconfig/cmake_install.cmake")
 
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/Users/aleixgelabert/Documents/MECATRÒNICA/TFM/SOFTWARE/CORE/COMANDAMENT_V4/build/esp-idf/mbedtls/mbedtls/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
